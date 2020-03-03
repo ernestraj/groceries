8c858ccb874629de82488fd2bb60d9b1ba2ae017
TajinderSingh Minhas <tajinders.minhas@gmail.com>
2020-03-02 16:59:41 -0800

    Adding chanegs for autocomplete

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
+import AutoComplete from "react-autocomplete";
 
 class SearchForm extends Component {
-	render() {
-		return (
-			<form className="form-inline my-2 my-lg-0">
-				<input
-					className="form-control mr-sm-2"
-					type="search"
-					placeholder="Search"
-					aria-label="Search"
-					onChange={this.onChange}
-				/>
-				<button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-					Search
-				</button>
-			</form>
-		);
-	}
+  state = {
+    value: ""
+  };
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+  onSelect = value => {
+    this.setState({ value: value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(event);
+  };
+  render() {
+    const { value } = this.state;
+    return (
+      <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
+        <AutoComplete
+          getItemValue={item => item.label}
+          items={[{ label: "apple" }, { label: "banana" }, { label: "pear" }]}
+          renderItem={(item, isHighlighted) => (
+            <div
+              key={item.label}
+              style={{ background: isHighlighted ? "lightgray" : "white" }}
+            >
+              {item.label}
+            </div>
+          )}
+          value={value}
+          onChange={this.onChange}
+          onSelect={this.onSelect}
+          placeholder="Search"
+        />
+
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
 }
 
 export default SearchForm;
